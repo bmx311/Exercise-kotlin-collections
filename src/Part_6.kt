fb3af4c3c972a4f2d1daea0f3607564d556199df
abdulwahab alsenan <ak-alsenan@hotmail.com>
2025-02-13 12:28:49 +0300

    part 6 done

---
+++ src/Part_6.kt
@@ -0,0 +1,5 @@
+fun main(){
+    val floatNumbers = listOf(2.5, 7.1, 3.8, 9.2, 5.6)
+    println("Largest number is: " + floatNumbers.maxOrNull())
+    println("Smallest number is: " + floatNumbers.minOrNull())
+}
